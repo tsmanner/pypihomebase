e0ade2071a417a0bc17b2cc3a854181e3404d616
tsmanner <zephrincochrane@gmail.com>
2016-02-19 16:45:16 -0500

    Split time and date to two lines with different font sizes and disabled time updates when it isn't being displayed.

--- pi_screen.py
+++ pi_screen.py
@@ -1,4 +1,4 @@
-import PIL
+import os
 import time
 import tkinter as tk
 
@@ -39,15 +39,38 @@ class HomeScreen(Layer):
 class HomeIdleScreen(Layer):
     def __init__(self, master):
         super().__init__(master, bg='black')
+        self.update_time_id = None
         self.time = tk.Label(self,
                              font="Ariel 80",
+                             fg='dark orange',
+                             bg='black',
+                             anchor=tk.N)
+        self.date = tk.Label(self,
+                             font="Ariel 40",
                              fg='dark orange',
                              bg='black',
                              anchor=tk.N)
         self.update_time()
 
     def update_time(self, event=None):
-        self.time.config(text=str(time.asctime()))
-        self.time.pack()
-        self.after(50, self.update_time)
+        self.time.config(text=time.strftime("%H:%M:%S"))
+        self.date.config(text=time.strftime("%A, %B %d %Y"))
+        self.time.pack(side=tk.TOP)
+        self.date.pack(side=tk.TOP)
+        self.update_time_id = self.after(50, self.update_time)
 
+    def place(self, *args, **kwargs):
+        super().place(*args, **kwargs)
+        self.update_time()
+
+    def place_forget(self):
+        super().place_forget()
+        self.after_cancel(self.update_time_id)
+
+    def pack(self, *args, **kwargs):
+        super().pack(*args, **kwargs)
+        self.update_time()
+
+    def pack_forget(self):
+        super().pack_forget()
+        self.after_cancel(self.update_time_id)
