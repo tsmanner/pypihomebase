c12536c0c33fb2936c0ab95a2d51c8cb46498655
tsmanner <zephrincochrane@gmail.com>
2016-02-22 01:00:38 -0500

    Trying red as clock color

--- pi_screen.py
+++ pi_screen.py
@@ -72,7 +72,7 @@ class HomeScreen(Layer):
 
 
 class HomeIdleScreen(tk.Toplevel):
-    TEXT_COLOR = "dark red"
+    TEXT_COLOR = "red"
 
     def __init__(self, master):
         super().__init__(master, bg='black')
