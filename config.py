7f4f857801e1485a4dcbbe690b49d2790614fac2
tsmanner <zephrincochrane@gmail.com>
2016-02-22 21:23:19 -0500

    Added clock color change based on time

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import json
 import os
 
-config = json.load(open("default.config"))
-if os.path.exists("local.config"):
-    local = json.load(open("local.config"))
+default_file = os.path.dirname(__file__) + os.sep + "default.config"
+local_file = os.path.dirname(__file__) + os.sep + "local.config"
+
+config = json.load(open(default_file))
+if os.path.exists(local_file):
+    local = json.load(open(local_file))
     config.update(local)
